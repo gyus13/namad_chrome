26fb52a7310005f8b8b3fe384449f96377b3ba05
gyus <gyusss13@naver.com>
2020-11-08 00:00:07 +0900

    third commit

---
+++ todo.js
@@ -0,0 +1,60 @@
+const toDoform = document.querySelector(".js-toDoForm"),
+ toDoInput = toDoform.querySelector("input"),
+ toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+const toDos = [];
+
+// 5. saveToDos
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); //object를 string으로 바꿔준다. json
+}
+
+// 4. paintToDo
+function paintTodo(text) {
+    const li = document.createElement("li"); // li를 추가함.
+    const delBtn = document.createElement("button");
+    const span = document.createElement("span");
+    const newId = toDos.length +1;
+    delBtn.innerText = "XXX";
+    span.innerText = text;
+    li.appendChild(delBtn);
+    li.appendChild(span); // li 에다 생성한 span 넣기
+    li.id = newId;
+    toDoList.appendChild(li);
+    const toDoObj = {
+        text : text,
+        id : newId
+    };
+    toDos.push(toDoObj); //배열에 추가해준다.
+    saveToDos();
+}
+
+// 3. handleSubmit
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintTodo(currentValue);
+    toDoInput.value = ""; // 입력한 뒤에 값 날리기
+}
+
+function something(toDo){
+    paintTodo(toDo.text);
+};
+
+// 2.loadTodos
+function loadTodos(){
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null){
+        const parsedToDos = JSON.parse(loadedToDos); // string을 object로 바꾼다.
+        parsedToDos.forEach(something);
+    }
+}
+
+// 1.init
+function init(){
+    loadTodos();
+    toDoform.addEventListener("submit",handleSubmit);
+}
+
+init();
